6bf634f0d76bcf8661a41f216322c411d1ab9b54
Özay Can Kırlı <ozaycank97@gmail.com>
2023-09-13 17:29:47 +0300

    CRUD Project Realized

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <ProductAdd @add:product="addProduct"></ProductAdd>
+    <ProductList
+      @delete:product="deleteProduct"
+      @update:product="updateProduct"
+      :products="products"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductList from "./components/ProductList.vue";
+import ProductAdd from "./components/ProductAdd.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ProductList,
+    ProductAdd,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  mounted() {
+    this.getProducts();
+  },
+  methods: {
+    async getProducts() {
+      const res = await fetch("http://localhost:3000/products");
+      const data = await res.json();
+      this.products = data;
+    },
+    async deleteProduct(product) {
+      await fetch('http://localhost:3000/products/' + product.id, {
+        method: "DELETE",
+      });
+      this.products = this.products.filter(
+        (productToFilter) => productToFilter.id != product.id
+      );
+    },
+    async updateProduct(product) {
+      const res = await fetch('http://localhost:3000/products/'+product.id, {
+        method: "PUT",
+        body: JSON.stringify(product),
+        headers: { "Content-Type": "application/json" },
+      })
+      const updatedProduct = await res.json();
+      this.products = this.products.map(product=>product.id===updatedProduct.id?updatedProduct:product)
+    },
+
+    async addProduct(product) {
+      const res = await fetch('http://localhost:3000/products', {
+        method: "POST",
+        body: JSON.stringify(product),
+        headers: { "Content-Type": "application/json" },
+      });
+      const newProduct = await res.json();
+      this.products = [...this.products, newProduct];
+    },
+  },
+};
 </script>
 
 <style>
@@ -19,7 +69,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
